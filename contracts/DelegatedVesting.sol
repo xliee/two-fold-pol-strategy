e2c8ab4f2c87580eebd0109c05dd6c42354abda0
samgos <samuel.jj.gosling@gmail.com>
2022-02-17 21:02:58 +0000

    DelegatedVesting draft

--- contracts/DelegatedVesting.sol
+++ contracts/DelegatedVesting.sol
@@ -1,25 +1,80 @@
 pragma solidity 0.8.0;
 
-import "./IERC20.sol";
+import "./interfaces/IERC20.sol";
+import "./interfaces/IGovernance.sol";
+
+contract DelegatedInstance {
+
+  address public spender;
+  address public sender;
+  uint256 public balance;
+
+  IGovernance governance;
+  IERC20 token;
+
+  constructor(
+    address delegationAddress,
+    address governanceAddress,
+    address tokenAddress,
+    uint256 stakeAmount,
+    uint256 deadline,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) {
+    governance = IGovernance(governanceAddress);
+    token = IERC20(tokenAddress);
+
+    token.transferFrom(msg.sender, stakeAmount);
+    lockAndDelegate(delegationAddress, deadline, v, r, s);
+
+    spender = delegationAddress;
+    balance = stakeAmount;
+    sender = msg.sender;
+  }
+
+  function delegate(address to) public {
+    require(msg.sender == spender, "Incorrect spender");
+
+    governance.delegate(to);
+  }
+
+  function lockAndDelegate(address to, deadline, v, r, s) internal {
+    governance.lock(
+      address(this), stakeAmount, deadline, v, r, s
+    );
+    governance.delegate(to);
+  }
+
+  function unlockAndRedeem() public {
+    require(msg.sender == sender, "Incorrect sender");
+
+    delete balances[msg.sender];
+
+    governance.unlock(balance);
+    token.transfer(spender, balance);
+  }
+
+}
 
 contract DelegatedVesting {
 
   uint256 public vestingPeriod;
-  address public vestingGovernance;
+  IGovernance public vestingGovernance;
   IERC20 public vestingToken;
 
   mapping(address => uint256) balances;
-  mapping(address => uint256) delegations;
+  mapping(address => address) delegations;
   mapping(address => uint256) commitments;
 
   constructor(
     uint256 vestingDuration,
     address governanceAddress,
-    address tokenAddress
+    address tokenAddress,
   ) {
     vestingPeriod = vestingDuration;
     vestingToken = IERC20(tokenAddress);
-    vestingGovernance = governanceAddress;
+    vestingGovernance = IGovernance(governanceAddress);
   }
 
   function isActiveCommitment(address stakeholderAddress) view returns (bool) {
@@ -33,7 +88,7 @@ contract DelegatedVesting {
     uint256 delegated = delegations[stakeholderAddress];
     bool state = isActiveCommitment(stakeholderAddress);
 
-    return state && delegated == stake;
+    return state && delegated != address(0x0);
   }
 
   function isFulfilledCommitment(address stakeholderAddress) view returns (bool) {
@@ -45,7 +100,7 @@ contract DelegatedVesting {
 
   function makeCommitment(
     address recipientAddress,
-    uint256 stakeAmount
+    uint256 stakeAmount,
   ) external returns (bool) {
     require(vestingToken.transferFrom(msg.sender, address(this), stakeAmount));
 
@@ -61,32 +116,42 @@ contract DelegatedVesting {
   }
 
   function delegateCommitment(
-    address candidateAddress
+    address candidateAddress,
+    uint256 deadline,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
   ) external {
     require(isActiveCommitment(msg.sender), "Not an active commitment");
 
-    if(!isDelegatedCommitment(msg.sender)) {
-      // @TODO governance lock
-      delegations[msg.sender] = balances[msg.sender];
+    if(isDelegatedCommitment(msg.sender)) {
+      DelegatedInstance(delegations[msg.sender]).delegate(candidateAddress);
+    } else {
+      DelegatedInstance e = new DelegatedInstance(
+        candidateAddress,
+        vestingGovernance,
+        address(vestingToken),
+        balances[msg.sender],
+        vestingPeriod,
+        deadline,
+        v, r, s
+     );
+     delegations[msg.sender] = address(e);
     }
-
-    // @TODO governance delegate
   }
 
-  function fulfilCommitment() external {
+  function fulfilCommitment() public {
     require(isFulfilledCommitment(msg.sender), "Commitment is not possible to fulfil");
 
     uint256 stake = balances[msg.sender];
     uint256 delegated = delegations[msg.sender];
 
     if(stake == delegated){
-      // @TODO governance withdraw
       delete delegations[msg.sender];
+      DelegatedInstance(delegations[msg.sender]).unlockAndRedeem();
     }
-
     delete balances[msg.sender];
-
-    require(vestingToken.transfer(msg.sender, stake));
+    vestingToken.transfer(msg.sender, stake);
   }
 
  }
---
+++ contracts/interfaces/IGovernance.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.80;
+
+interface IGovernance  {
+
+  function lock(
+    address owner,
+    uint256 amount,
+    uint256 deadline,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) external;
+
+  function delegate(address to) external;
+
+  function undelegate() external;
+
+}
